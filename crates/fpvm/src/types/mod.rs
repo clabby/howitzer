0124d70c7adbd680eb868a6b83ef3f15c52c54f1
clabby <ben@clab.by>
2024-07-28 22:48:47 -0400

    syscall fix (MIPS64)

--- crates/fpvm/src/types/mod.rs
+++ crates/fpvm/src/types/mod.rs
@@ -71,12 +71,12 @@ impl TryFrom<u8> for Fd {
 
 /// A [Syscall] is a system call that can be made within the MIPS emulator.
 pub enum Syscall {
-    Mmap = 4090,
-    Brk = 4045,
-    Clone = 4120,
-    ExitGroup = 4246,
-    Read = 4003,
-    Write = 4004,
+    Mmap = 5009,
+    Brk = 5012,
+    Clone = 5055,
+    ExitGroup = 5205,
+    Read = 5000,
+    Write = 5001,
     Fcntl = 4055,
 }
 
@@ -85,12 +85,12 @@ impl TryFrom<u64> for Syscall {
 
     fn try_from(n: u64) -> Result<Self, Self::Error> {
         match n {
-            4090 => Ok(Syscall::Mmap),
-            4045 => Ok(Syscall::Brk),
-            4120 => Ok(Syscall::Clone),
-            4246 => Ok(Syscall::ExitGroup),
-            4003 => Ok(Syscall::Read),
-            4004 => Ok(Syscall::Write),
+            5009 => Ok(Syscall::Mmap),
+            5012 => Ok(Syscall::Brk),
+            5055 => Ok(Syscall::Clone),
+            5205 => Ok(Syscall::ExitGroup),
+            5000 => Ok(Syscall::Read),
+            5001 => Ok(Syscall::Write),
             4055 => Ok(Syscall::Fcntl),
             _ => anyhow::bail!("Failed to convert {} to Syscall", n),
         }
